6cb699153f302308675639e8fc8e13b994a39453
J B <ironviralmedia@gmail.com>
2018-02-05 18:30:19 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,9 +2,13 @@
 katz_deli = []
 
 def line(katz_deli)
-  # if !katz_deli
-  #   return "empty"
-  # end
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+
+    katz_deli.each_with_index do |person, i|
+    puts "The line is currently: #{i + 1}. #{person}"
+  end
 end
 
 def take_a_number(katz_deli, name)
