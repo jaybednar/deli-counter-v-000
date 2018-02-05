0e8d7df02790a4a419093512357bca3c137b7f61
J B <ironviralmedia@gmail.com>
2018-02-05 18:22:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  # if !katz_deli
+  #   return "empty"
+  # end
+end
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving(katz_deli)
+
+end
